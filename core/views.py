919c333093b268fd9dd8206c2126f7d622ac32c5
Todd <todd@MacBook-Pro.local>
2026-03-09 22:42:36 -0400

    added home view. The html files don't exist yet.

--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from core.models import Membership
+
+
 # Create your views here.
+@login_required(login_url="/login/")
+def home(request):
+    membership = Membership.objects.filter(user=request.user).first()
+
+    if membership.status == Membership.status.PENDING:
+        return render(request, 'core/pending-approval.html')
+    if membership.status == Membership.status.REJECTED:
+        return render(request, "core/rejected.html", )
+    if membership.status == Membership.status.BANNED:
+        return render(request, "core/banned.html")
+    if membership.status == Membership.status.APPROVED:
+        return render(request, "/")
+    return render(request, 'core/org-page.html')
