b456e7462f90f15f55a8e09c481a34c7aed94a02
Todd <todd@MacBook-Pro.local>
2026-03-09 22:16:09 -0400

    added membership model with one-to-one User model relationship

--- core/models.py
+++ core/models.py
@@ -5,3 +5,23 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+class Membership(models.Model):
+    class Role(models.IntegerChoices):
+        OWNER = 1
+        ADMIN = 2
+        MEMBER = 3
+
+    class Status(models.TextChoices):
+        PENDING = "pending"
+        APPROVED = "approved"
+        REJECTED = "rejected"
+        BANNED = "banned"
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.IntegerField(choices=Role.choices, default=Role.MEMBER)
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.PENDING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
