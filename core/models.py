89393616fbe575e5af5cb0070c863757e69c6b64
Todd <todd@MacBook-Pro.local>
2026-03-09 21:44:24 -0400

    Added Organization model

--- core/models.py
+++ core/models.py
@@ -4,3 +4,11 @@ from django.db import models
 # Create your models here.
 class User(AbstractUser):
     pass
+
+class Organization(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
