ed128efec63d6a5472254e7b0dd33f1cbf5b5eca
Quintin Levy <159190598+quintinlevy@users.noreply.github.com>
2026-03-12 21:48:37 -0400

    fixed exec panel button appearing for banned execs, fixed syntax error with profile url

--- project_a_17/urls.py
+++ project_a_17/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 from django.urls.conf import include
 
 urlpatterns = [
