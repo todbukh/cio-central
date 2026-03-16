99fcbf04c74c5568a0e5015e1bb096d2d802d93a
Todd <todd@MacBook-Pro.local>
2026-03-16 19:22:21 -0400

    added roster controller urls to roster/urls.py

--- roster/urls.py
+++ roster/urls.py
@@ -6,4 +6,7 @@ app_name = 'roster'
 
 urlpatterns = [
     path("", views.roster, name="roster"),
+    path("action/accept/<int:pk>/", views.accept, name="accept"),
+    path("action/reject/<int:pk>/", views.reject, name="reject"),
+    path("action/ban/<int:pk>/", views.ban, name="ban"),
 ]
