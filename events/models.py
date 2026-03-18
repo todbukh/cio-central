98c7de55b6ad5b47c75d4702bdd8481fc77a5009
Todd <todd@MacBook-Pro.local>
2026-03-18 13:10:55 -0400

    made dummy Event model with name, date, and description fields for testing

--- events/models.py
+++ events/models.py
@@ -1,5 +1,11 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Event(models.Model):
-    pass
+    name = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
