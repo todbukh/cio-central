191269435bc5d487235267131b165cf48a704dcf
Todd <todd@MacBook-Pro.local>
2026-03-18 08:31:42 -0400

    Set up attendance model and stubbed the events model for now.

--- attendance/models.py
+++ attendance/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from core.models import User
+from events.models import Event
 
 # Create your models here.
+class Attendance(models.Model):
+    class Status(models.TextChoices):
+        PRESENT = "PRESENT"
+        ABSENT = "ABSENT"
+        EXCUSED = "EXCUSED"
+        UNSET = "UNSET"
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE) #FIXME: Talk to Quintin. This is a dummy model for now
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.UNSET)
