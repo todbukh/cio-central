f017f71dc93716cd7520a6577d8724bef3107546
voyager7 <kenningspath@gmail.com>
2026-03-21 17:06:57 -0400

    added actual message form on frontend and backend + made textarea automatically resizing with error message when too much text is input. form validation happens on frontend and backend

--- organization/static/organization/home.js
+++ organization/static/organization/home.js
@@ -1,7 +1,7 @@
 // Credit to Gemini 3.1 Pro for suggesting using JS for this (and for showing me some of this logic, though I wrote this myself)
 
 // adding this event listener is likely overkill, but it just ensures this won't fire until the DOM is fully loaded
-addEventListener("DOMContentLoaded", (_event) => {
+document.addEventListener("DOMContentLoaded", (_event) => {
     const messageContainer = document.getElementById("messageContainer");
 
     // Reference for how this work:
@@ -10,3 +10,36 @@ addEventListener("DOMContentLoaded", (_event) => {
     // https://developer.mozilla.org/en-US/docs/Web/API/Element/clientHeight
     messageContainer.scrollBy(0, messageContainer.scrollHeight - messageContainer.clientHeight);
 });
+
+// Credit to Google AI overview for suggesting using the keyup event listener to display an error message when message input is too long
+// I wrote this myself, though
+document.addEventListener("keyup", (event) => {
+    displayInputErrorMessage();
+})
+
+document.addEventListener("keydown", (event) => {
+    displayInputErrorMessage();
+})
+
+function displayInputErrorMessage() {
+    const messageTextBox = document.getElementById("messageTextBox");
+    const messageInputError = document.getElementById("messageInputError");
+    const messageSubmitButton = document.getElementById("messageSubmitButton");
+    const messageComposerContainer = document.getElementById("messageComposerContainer");
+
+    // learned how to make this auto-resizing from here:
+    // https://stackoverflow.com/questions/454202/creating-a-textarea-with-auto-resize
+    messageTextBox.style.height = 'auto';
+    if (messageTextBox.scrollHeight < 200) messageTextBox.style.height = messageTextBox.scrollHeight + "px";
+    else messageTextBox.style.height = "200px";
+
+    if (messageTextBox.value.length > 2000){
+        messageInputError.className = "text-danger mb-0";  // show error message
+        messageSubmitButton.setAttribute("disabled", "true");  // disable send button
+        messageComposerContainer.className = "px-3 px-md-4 border-top bg-body flex-shrink-0 pt-4";  // remove bottom padding to prevent visual shift
+    } else {
+        messageInputError.className = "text-danger mb-0 d-none";
+        messageSubmitButton.removeAttribute("disabled");
+        messageComposerContainer.className = "px-3 px-md-4 border-top bg-body flex-shrink-0 pt-4 pb-4";
+    }
+}
--- organization/views.py
+++ organization/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
+
+from .forms import MessageForm
 from .models import *
 
 
@@ -10,7 +12,17 @@ def home_redirect(request):
 
 @login_required(login_url="/login/")
 def messages(request, channel):
-    get_object_or_404(Channel, name=channel)
+    active_channel_model = get_object_or_404(Channel, name=channel)
+
+    if request.method == "POST":
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message = Message(channel=active_channel_model, user=request.user, text=form.cleaned_data["text"])
+            message.save()
+            return redirect("organization:messages", channel=channel)
+        else:
+            message_send_error = "Messages must be 2000 characters or less"
+            form_text = form["text"].data
 
     message_list = list(Message.objects.all().filter(channel__name=channel))
     message_list.sort(key=lambda message : message.sent_at)
@@ -18,7 +30,7 @@ def messages(request, channel):
     context = {
         "active_channel": channel,
         "channels": list(Channel.objects.all()),
-        "messages": message_list
+        "messages": message_list,
     }
 
     return render(request, 'organization/home.html', context)
