a1f232f27f0d03acfcdc20e88407bac763cfbce7
voyager7 <kenningspath@gmail.com>
2026-03-21 20:26:55 -0400

    made exec_only channels read-only for non-execs. also passed that data with data-* attributes into the script to avoid re-enabling the send button for non-execs

--- organization/static/organization/home.js
+++ organization/static/organization/home.js
@@ -46,7 +46,9 @@ function displayInputErrorMessage(messageTextBox, inputErrorMessageElement, mess
         messageComposerContainer.style.paddingBottom = "0";
     } else {
         inputErrorMessageElement.className = "text-danger d-none";  // hide error message
-        messageSubmitButton.removeAttribute("disabled");  // re-enable send button
+        if (messageSubmitButton.getAttribute("data-exec-only") !== "true" || messageSubmitButton.getAttribute("data-user-is-exec") === "true") {  // don't re-enable if page is exec only
+            messageSubmitButton.removeAttribute("disabled");  // re-enable send button
+        }
         messageComposerContainer.style.paddingBottom = "30px";  // replace padding on bottom
     }
 }
--- organization/views.py
+++ organization/views.py
@@ -12,12 +12,12 @@ def home_redirect(request):
 
 @login_required(login_url="/login/")
 def messages(request, channel):
-    active_channel_model = get_object_or_404(Channel, name=channel)
+    active_channel = get_object_or_404(Channel, name=channel)
 
     if request.method == "POST":
         form = MessageForm(request.POST)
         if form.is_valid():
-            message = Message(channel=active_channel_model, user=request.user, text=form.cleaned_data["text"])
+            message = Message(channel=active_channel, user=request.user, text=form.cleaned_data["text"])
             message.save()
             return redirect("organization:messages", channel=channel)
         else:
@@ -28,7 +28,7 @@ def messages(request, channel):
     message_list.sort(key=lambda message : message.sent_at)
 
     context = {
-        "active_channel": channel,
+        "active_channel": active_channel,
         "channels": list(Channel.objects.all()),
         "messages": message_list,
     }
