5c46f36740c9846f3f7ed8f445a6b6033d88ff6d
Todd <todd@MacBook-Pro.local>
2026-04-08 08:37:16 -0400

    view created for handling user deletion

--- core/views.py
+++ core/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
+
+from .models import User
 
 
 # Create your views here.
@@ -7,3 +11,10 @@ def login(request):
     if request.user.is_authenticated:
         return redirect("/")
     return render(request, "core/login.html")
+
+@require_POST
+@login_required
+def delete_user(request, uid):
+    user = get_object_or_404(User, uid=uid)
+    user.delete()
+    return redirect("/")
