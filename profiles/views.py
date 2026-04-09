454da6d6f6fb1e56352235f4e0e9ca146628076f
voyager7 <kenningspath@gmail.com>
2026-04-09 01:24:34 -0400

    removed incorrect rogue import added by ide

--- profiles/views.py
+++ profiles/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 
-from core.models import Role
 from .forms import ProfileEditForm
 from django.core.files.storage import default_storage
 
