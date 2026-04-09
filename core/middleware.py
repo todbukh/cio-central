4dc6e2a7ba0ebaa01cbcae21b6aa90da687ad0dc
voyager7 <kenningspath@gmail.com>
2026-04-09 01:36:56 -0400

    corrected circular redirect middleware bug

--- core/middleware.py
+++ core/middleware.py
@@ -45,6 +45,8 @@ class ApprovalStatusMiddleware:
         if request.user.is_user_admin():
             if not request.path.startswith("/user-admin/") and not request.path.startswith("/admin/"):
                 return redirect("user_admin:user_admin")
+            # short-circuit middleware here because status checks don't apply to USERADMINs
+            return self.get_response(request)
 
         status_path = self._status_path(request.user)
         if status_path is not None:
