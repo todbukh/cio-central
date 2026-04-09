15f3ad08ee767adaceea7a4f078377ec019aa74e
voyager7 <kenningspath@gmail.com>
2026-04-09 03:47:59 -0400

    renamed is_user_admin import in core models to avoid confusion

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ import uuid
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from core.permissions import is_executive, is_user_admin, is_owner as check_is_owner
+from core.permissions import is_executive, is_user_admin as check_is_user_admin, is_owner as check_is_owner
 
 # Create your models here.
 class User(AbstractUser):
@@ -29,7 +29,7 @@ class User(AbstractUser):
         return check_is_owner(self)
 
     def is_user_admin(self):
-        return is_user_admin(self)
+        return check_is_user_admin(self)
 
     def __str__(self):
         return self.username
