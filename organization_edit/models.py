40d93600323e4a9551a8c1135541b59a98278646
voyager7 <kenningspath@gmail.com>
2026-04-24 15:58:28 -0400

    made max length for name bigger

--- organization_edit/models.py
+++ organization_edit/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Organization(models.Model):
     id = models.IntegerField(primary_key=True, default=0)
-    name = models.CharField(max_length=20, default="My Organization")
+    name = models.CharField(max_length=255, default="My Organization")
     organization_picture = models.ImageField(upload_to="images/organization-edit/", blank=True, null=True)
